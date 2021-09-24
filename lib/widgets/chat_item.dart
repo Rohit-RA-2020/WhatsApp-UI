1ad79ba5fd699381ac3d396e1c6a517219688ceb
Rohit ranjan <ranjanrohit812@gmail.com>
2021-09-24 10:49:47 +0530

    Added Divider b/w chats

--- lib/widgets/chat_item.dart
+++ lib/widgets/chat_item.dart
@@ -18,27 +18,36 @@ class ChatItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 3.0),
-      child: ListTile(
-        leading: const CircleAvatar(
-          backgroundColor: Colors.teal,
-          backgroundImage: NetworkImage(kUrl),
-        ),
-        title: Text(
-          name,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Row(
-          children: <Widget>[
-            const Icon(
-              Icons.done_all,
-              size: 15,
-              color: Colors.white38,
+      child: Column(
+        children: [
+          ListTile(
+            leading: const CircleAvatar(
+              backgroundColor: Colors.teal,
+              backgroundImage: NetworkImage(kUrl),
             ),
-            const SizedBox(width: 4),
-            Text(message, style: const TextStyle(color: Colors.white38))
-          ],
-        ),
-        trailing: Text(time, style: const TextStyle(color: Colors.white38)),
+            title: Text(
+              name,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Row(
+              children: <Widget>[
+                const Icon(
+                  Icons.done_all,
+                  size: 15,
+                  color: Colors.white38,
+                ),
+                const SizedBox(width: 4),
+                Text(message, style: const TextStyle(color: Colors.white38))
+              ],
+            ),
+            trailing: Text(time, style: const TextStyle(color: Colors.white38)),
+          ),
+          const Divider(
+            height: 1,
+            indent: 10,
+            endIndent: 10,
+          )
+        ],
       ),
     );
   }

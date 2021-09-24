a11669cc7588ff73cccc6def9edd0a9119aaecf4
Rohit ranjan <ranjanrohit812@gmail.com>
2021-09-24 11:38:27 +0530

    Minor FAB Bug fix

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -31,103 +31,106 @@ class _HomeScreenState extends State<HomeScreen>
     return DefaultTabController(
       length: 4,
       child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            title: const Text(
-              'WhatsApp',
-              style: TextStyle(color: kAppBarTextColor),
-            ),
-            actions: <IconButton>[
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.search,
-                  color: kAppBarTextColor,
-                ),
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(color: kAppBarTextColor),
+          ),
+          actions: <IconButton>[
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: kAppBarTextColor,
               ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.more_vert,
-                  color: kAppBarTextColor,
-                ),
-              )
-            ],
-            bottom: TabBar(
-              controller: tabController,
-              unselectedLabelColor: Colors.grey,
-              indicatorSize: TabBarIndicatorSize.label,
-              labelColor: kTabTextColor,
-              tabs: const <Tab>[
-                Tab(icon: Icon(Icons.camera_alt)),
-                Tab(text: 'CHATS'),
-                Tab(text: 'STATUS'),
-                Tab(text: 'CALLS'),
-              ],
             ),
-          ),
-          body: TabBarView(
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: kAppBarTextColor,
+              ),
+            )
+          ],
+          bottom: TabBar(
             controller: tabController,
-            children: const <Widget>[
-              CameraScreen(),
-              ChatScreen(),
-              StatusScreen(),
-              CallScreen(),
+            unselectedLabelColor: Colors.grey,
+            indicatorSize: TabBarIndicatorSize.label,
+            labelColor: kTabTextColor,
+            tabs: const <Tab>[
+              Tab(icon: Icon(Icons.camera_alt)),
+              Tab(text: 'CHATS'),
+              Tab(text: 'STATUS'),
+              Tab(text: 'CALLS'),
             ],
           ),
-          floatingActionButton: tabController!.index == 1
-              ? FloatingActionButton(
-                  backgroundColor: kFabColor,
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/newChat');
-                  },
-                  child: const Icon(
-                    Icons.message_rounded,
-                    color: Colors.white,
-                  ),
-                )
-              : tabController!.index == 2
-                  ? Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SizedBox(
-                          height: 45,
-                          width: 45,
-                          child: FloatingActionButton(
-                            elevation: 2,
-                            backgroundColor: Colors.black,
+        ),
+        body: TabBarView(
+          controller: tabController,
+          children: const <Widget>[
+            CameraScreen(),
+            ChatScreen(),
+            StatusScreen(),
+            CallScreen(),
+          ],
+        ),
+        floatingActionButton: tabController!.index == 0
+            ? null
+            : tabController!.index == 1
+                ? FloatingActionButton(
+                    backgroundColor: kFabColor,
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/newChat');
+                    },
+                    child: const Icon(
+                      Icons.message_rounded,
+                      color: Colors.white,
+                    ),
+                  )
+                : tabController!.index == 2
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: 45,
+                            width: 45,
+                            child: FloatingActionButton(
+                              elevation: 2,
+                              backgroundColor: Colors.black,
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/newChat');
+                              },
+                              child: const Icon(
+                                Icons.edit,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          FloatingActionButton(
+                            backgroundColor: kFabColor,
                             onPressed: () {
                               Navigator.pushNamed(context, '/newChat');
                             },
                             child: const Icon(
-                              Icons.edit,
+                              Icons.camera_alt,
                               color: Colors.white,
                             ),
                           ),
+                        ],
+                      )
+                    : FloatingActionButton(
+                        backgroundColor: kFabColor,
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/newChat');
+                        },
+                        child: const Icon(
+                          Icons.add_call,
+                          color: Colors.white,
                         ),
-                        const SizedBox(height: 10),
-                        FloatingActionButton(
-                          backgroundColor: kFabColor,
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/newChat');
-                          },
-                          child: const Icon(
-                            Icons.camera_alt,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    )
-                  : FloatingActionButton(
-                      backgroundColor: kFabColor,
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/newChat');
-                      },
-                      child: const Icon(
-                        Icons.add_call,
-                        color: Colors.white,
                       ),
-                    )),
+      ),
     );
   }
 }

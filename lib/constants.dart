7f4dba3ae06cadb530acdce5d79facd481779199
Rohit ranjan <ranjanrohit812@gmail.com>
2021-09-27 15:44:19 +0530

    Camera Controls Added

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,3 +7,5 @@ const Color kTabTextColor = Colors.tealAccent;
 const Color kFabColor = Color(0xFF128C7E);
 
 const String kUrl = 'https://thispersondoesnotexist.com/image';
+
+const double kIconSize = 30.0;
--- lib/screens/camera.dart
+++ lib/screens/camera.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/constants.dart';
 
-class CameraScreen extends StatelessWidget {
+class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
 
   @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  bool flashPressed = false;
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Camera Screen'),
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: const Color(0xFF131C21),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(flashPressed ? Icons.flash_off : Icons.flash_on,
+                      size: kIconSize),
+                  onPressed: () {
+                    flashToggle();
+                  },
+                ),
+                const Icon(Icons.circle_outlined, size: 60),
+                const Icon(Icons.flip_camera_android, size: kIconSize)
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Hold for video, tap for photo'),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  void flashToggle() {
+    if (flashPressed) {
+      setState(() {
+        flashPressed = false;
+      });
+    } else {
+      setState(() {
+        flashPressed = true;
+      });
+    }
   }
 }

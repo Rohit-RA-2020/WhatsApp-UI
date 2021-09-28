ba7aef5f405c51fe6ab37b59719456f3602804c4
Rohit ranjan <ranjanrohit812@gmail.com>
2021-09-28 13:42:32 +0530

    Added textFiled input to chat screen

--- lib/details/chat_conversation.dart
+++ lib/details/chat_conversation.dart
@@ -1,56 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_ui/constants.dart';
 import 'package:whatsapp_ui/details/calling_screen.dart';
+import 'package:whatsapp_ui/widgets/chat_input_field.dart';
+
+import '../constants.dart';
 
 class ChatConversation extends StatelessWidget {
-  final String name;
+  ChatConversation({required this.name});
 
-  const ChatConversation({Key? key, required this.name}) : super(key: key);
+  String name;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        actions: <IconButton>[
-          IconButton(onPressed: () {}, icon: const Icon(Icons.videocam)),
-          IconButton(
-              onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CallingScreen(name: name),
-                    ),
-                  ),
-              icon: const Icon(Icons.phone)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
-        ],
-        title: Row(
-          children: <Widget>[
-            const CircleAvatar(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                name,
-                style: const TextStyle(fontSize: 16),
-              ),
-            )
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Row(
+            children: <Widget>[
+              const BackButton(),
+              const CircleAvatar(backgroundImage: NetworkImage(kUrl)),
+              const SizedBox(width: 20.0 * 0.75),
+              Column(
+                children: <Widget>[
+                  Text(name, style: const TextStyle(fontSize: 16)),
+                  const Text('Active 3 min ago',
+                      style: TextStyle(fontSize: 10)),
+                ],
+              )
+            ],
+          ),
+          actions: <Widget>[
+            IconButton(
+                icon: const Icon(Icons.call),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CallingScreen(name: name)));
+                }),
+            IconButton(icon: const Icon(Icons.videocam), onPressed: () {}),
+            IconButton(icon: const Icon(Icons.more_vert), onPressed: () {})
           ],
         ),
-      ),
-      body: Stack(
-        children: [
-          Image.network(
-            'https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png',
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            fit: BoxFit.cover,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
+        body: Stack(
+          children: <Widget>[
+            Image.network(
+              'https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png',
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.cover,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Container(
                     height: 30,
                     width: 140,
@@ -65,71 +68,10 @@ class ChatConversation extends StatelessWidget {
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 350,
-                        child: Center(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: const <Widget>[
-                                    Icon(
-                                      Icons.face_rounded,
-                                      color: kAppBarTextColor,
-                                    ),
-                                    SizedBox(width: 6),
-                                    Text(
-                                      'Message',
-                                      style: TextStyle(color: kAppBarTextColor),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: const <Widget>[
-                                    Icon(Icons.attachment_rounded,
-                                        color: kAppBarTextColor),
-                                    SizedBox(width: 10),
-                                    Icon(Icons.camera_alt,
-                                        color: kAppBarTextColor)
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF262D31),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      const SizedBox(width: 5.0),
-                      const CircleAvatar(
-                        backgroundColor: Colors.teal,
-                        child: Icon(
-                          Icons.mic,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+                const ChatInputField()
+              ],
+            ),
+          ],
+        ));
   }
 }

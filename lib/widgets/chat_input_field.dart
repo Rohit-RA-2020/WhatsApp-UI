aeadc4996c4fb028d4cbccc09ce367ada18a553d
Rohit ranjan <ranjanrohit812@gmail.com>
2021-10-10 00:00:56 +0530

    Fixed Expanded Widget Exception

--- lib/widgets/chat_input_field.dart
+++ lib/widgets/chat_input_field.dart
@@ -36,42 +36,40 @@ class ChatInputField extends StatelessWidget {
                   color: Colors.teal.withOpacity(0.05),
                   borderRadius: BorderRadius.circular(40),
                 ),
-                child: Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.sentiment_satisfied_alt_outlined,
-                        color: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .color!
-                            .withOpacity(0.64),
-                      ),
-                      hintText: "Type message",
-                      border: InputBorder.none,
-                      suffixIcon: Row(
-                        children: [
-                          Icon(
-                            Icons.attach_file,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .color!
-                                .withOpacity(0.64),
-                          ),
-                          Icon(
-                            Icons.camera_alt_outlined,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .color!
-                                .withOpacity(0.64),
-                          ),
-                        ],
-                      ),
-                      suffixIconConstraints:
-                          const BoxConstraints(maxHeight: 48, maxWidth: 48),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.sentiment_satisfied_alt_outlined,
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .color!
+                          .withOpacity(0.64),
                     ),
+                    hintText: "Type message",
+                    border: InputBorder.none,
+                    suffixIcon: Row(
+                      children: [
+                        Icon(
+                          Icons.attach_file,
+                          color: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .color!
+                              .withOpacity(0.64),
+                        ),
+                        Icon(
+                          Icons.camera_alt_outlined,
+                          color: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .color!
+                              .withOpacity(0.64),
+                        ),
+                      ],
+                    ),
+                    suffixIconConstraints:
+                        const BoxConstraints(maxHeight: 48, maxWidth: 48),
                   ),
                 ),
               ),
